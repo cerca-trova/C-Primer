3eeb5e765ab078960bd836ed9cc4eb7434dad78e
chenran <ce88808@gmail.com>
2023-03-07 20:21:32 +0800

    starting to learn chapter3

--- chapter2/float_type.c
+++ chapter2/float_type.c
@@ -2,9 +2,15 @@
 
 int main(void){
     /* A float type demo,include long and long double. */
-    float float_type = 10e37;
+    float float_type = 10e36;
 
     /* Float type's display specification */
     printf("dispaly float type using %%f specification.");
-    printf("%f",float_type);
+    printf("%f\n",float_type);
+
+
+    /* Overflow and underflow of a float type */
+    float toobig = 3.4E38 * 100.0f;
+    printf("%e\n",toobig);
+
 }
